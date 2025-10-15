06d1667cf8da49ba7b7e98605479439ac36059e7
Nuntawat Saehuam <nanthawat.s@kkumail.com>
2025-10-15 20:13:39 +0700

    chore: trigger render auto-deploy

---
+++ apps/backend/src/deploy-trigger.js
@@ -0,0 +1,5 @@
+// trigger render at 2025-10-15T00:00:00.000Z
+// This file is a small placeholder used to trigger an automated deploy when the repo is updated.
+// Do not store secrets here.
+
+// noop
